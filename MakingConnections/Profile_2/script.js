4b7523bd78edef30c3176cb58b5af0cff706ff4e
Dashawn <creswellshawn@gmail.com>
2022-07-25 13:35:19 -0700

    Making Connections July 25

---
+++ MakingConnections/Profile_2/script.js
@@ -0,0 +1,18 @@
+function hide(element, element2, element3){
+    element.remove()
+    element2.innerText++
+    element3.innerText--
+}
+
+function hide2(element, element2){
+    element.remove()
+    element2.innerText--
+}
+
+function change(element) {
+    element.innerText = "Billy Jean"
+}
+
+// function likes(element) {
+//     element.innerText++;
+// }
