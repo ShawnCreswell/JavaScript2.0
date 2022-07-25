bf5dbe364111caf44d3c16cf4c21a8b604a97d22
Dashawn <creswellshawn@gmail.com>
2022-07-25 16:38:38 -0700

    DojoWeather July 25th

---
+++ DojoWeather/script.js
@@ -0,0 +1,30 @@
+function cityB(element){
+    alert('Burbank was Selected')
+}
+
+function cityC(element){
+    alert('Chicago was Selected')
+}
+
+function cityD(element){
+    alert('Dallas was Selected')
+}
+
+function hide(element){
+    element.remove()
+}
+
+function picked(element){
+    console.log(element.value)
+    alert('Loading Weather report in  ' + element.value)
+    // IF THE TEMP IS SET TO FAHRENHEIT
+    if(element.id == fah (red1)){
+        element.innerText = '75.2'
+    }
+    // IF THE TEMP IS SET TO CELSIUS
+
+}
+
+// function picked(element){
+//     element.innerText = '75.2'
+// }
