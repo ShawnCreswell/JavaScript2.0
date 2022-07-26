6c4ca16cccf0cc8a8e9929b8cdebac54906ce218
Dashawn <creswellshawn@gmail.com>
2022-07-26 13:13:25 -0700

    BeltExamReview

---
+++ BeltExamReview/script.js
@@ -0,0 +1,16 @@
+function buy(element) {
+    element.innerText++;
+}
+
+function switchPic(element) {
+    element.src = "./Images/stonepunk.png"
+}
+
+function switchPic2(element) {
+    element.src = "./Images/pixel-ninjas-2.png"
+}
+
+function linx(element){
+    alert('The game is supported on Linux')
+}
+
--- DojoWeather/script.js
+++ DojoWeather/script.js
@@ -15,21 +15,21 @@ function hide(element) {
 }
 
 function picked(element) {
-    alert('Loading Weather report in  ' + element.value)
+    // alert('Loading Weather report in  ' + element.value)
     // IF THE TEMP IS SET TO FAHRENHEIT
     if (element.value == 'F') {
         var degrees = document.getElementsByClassName('degree')
 
         for (var i = 0; i < degrees.length; i++) {
             console.log(degrees[i].innerText);
-            degrees[i].innerText = Math.floor(degrees[i].innerText * 1.8 + 32);
+            degrees[i].innerText = (((parseInt(degrees[i].innerText) * 1.8) + 32)).toFixed();
         }
     }
     else {
         var degrees = document.getElementsByClassName('degree')
         for(var i = 0;  i < degrees.length; i++){
             console.log(degrees[i].innerText);
-            degrees[i].innerText = Math.floor((degrees[i].innerText - 32) * .5556);
+            degrees[i].innerText = (((parseInt(degrees[i].innerText) - 32) / 1.8)).toFixed();
         }
     }
 }
